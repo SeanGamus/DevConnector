2ef6271e47e6e2d41b46a61791e5ec2b61d2a894
AQ\11780 <aditya.singh1@kantar.com>
2022-07-16 01:50:23 +0530

    routes added

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const connectDB = require('./config/db');
 const app = express();
 connectDB();
 
-app.get('/',(req,res)=>res.send('API Running'))
+app.get('/',(req,res)=>res.send('API Running'));
+
+app.use('/api/user',require('./routes/api/user'))
+app.use('/api/profile',require('./routes/api/profile'))
+app.use('/api/post',require('./routes/api/post'))
+app.use('/api/auth',require('./routes/api/auth'))
 
 const PORT = process.env.PORT || 5000;
 
